d60cf0baa54df967b34cb18b11b9de6fde7e5682
m0bi <poubelle.m0billard.1997@gmail.com>
2023-04-30 18:47:24 +0200

    CSS of the popup and README done

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -118,8 +118,7 @@
                                                         d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                                 </svg>
                                                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                                    Are you sure you want to delete
-                                                    this product?</h3>
+                                                    Êtes-vous sûr de vouloir supprimer ce produit ?</h3>
                                                 <form action="{{ route('products.destroy', $product->id) }}"
                                                     method="POST">
                                                     @csrf
@@ -128,9 +127,6 @@
                                                         class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
                                                         type="submit" data-modal-hide="popup-modal">Supprimer</button>
                                                 </form>
-                                                <button data-modal-hide="popup-modal" type="button"
-                                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                                                    cancel</button>
                                             </div>
                                         </div>
                                     </div>
